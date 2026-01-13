f9daddcfaf0253abd91ad34d740e57306b1022c9
Kalle Jonsson <jonsson.carljohan@gmail.com>
2026-01-13 22:47:43 +0100

    Cleanup

--- check_env.py
+++
@@ -1,4 +0,0 @@
-import os
-print("API_URL:", "Set" if os.environ.get("CANVAS_API_URL") else "Missing")
-print("API_TOKEN:", "Set" if os.environ.get("CANVAS_API_TOKEN") else "Missing")
-print("Course ID:", "1434 (Hardcoded in script)")
